ff460ce452e242c4811ffe9e3c1b1e9d2db57720
trymvalebjorg <trymvalebjorg@gmail.com>
2020-05-13 16:02:10 +0200

    Built html/css framework for lists and tasks, and added some js funcionality

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+// ReadMore for task
+let toggler = document.getElementsByClassName("list__item__expand");
+for( i = 0; i < toggler.length; i++){
+    toggler[i].addEventListener('click', function() {
+        this.parentElement.querySelector('.list__item__expanded').classList.toggle('active');
+        this.classList.toggle('arrow-down');
+    })
+}
