59aafb9e6efded3780209354689c821fd946d836
emi-louise97 <emilouise1997@hotmail.com>
2020-05-19 17:10:06 +0200

    Fixed leap year in datepicker, added timepicker. Not finished.

--- js/datepicker.js
+++ js/datepicker.js
@@ -1,58 +1,76 @@
-//Henter alle nødvendige HTML-elementer 
+//Henter klokkeslett-elementer fra HTML
+const timepickerElement = document.querySelector('.timepicker');
+let time = timepickerElement.dataset.time;
+const hrElement = document.querySelector('.timepicker__hour__hr');
+const minElement = document.querySelector('.timepicker__minute__min');
+const hrUpElement = document.querySelector('.timepicker__hour__hr-up');
+const hrDownElement = document.querySelector('.timepicker__hour__hr-down');
+const minUpElement = document.querySelector('.timepicker__minute__min-up');
+const minDownElement = document.querySelector('.timepicker__minute__min-down');
+
+//Henter dato-elementer fra HTML
 const datepickerElement = document.querySelector('.datepicker');
-const selectedDateElement = document.querySelector('.datepicker .datepicker__dateselected');
-const datesElement = document.querySelector('.datepicker .datepicker__dates');
-const mthElement = document.querySelector('.datepicker .datepicker__dates .datepicker__dates__month .datepicker__dates__month__mth');
-const nextMthElement = document.querySelector('.datepicker .datepicker__dates .datepicker__dates__month .datepicker__dates__month__nextmth');
-const prevMthElement = document.querySelector('.datepicker .datepicker__dates .datepicker__dates__month .datepicker__dates__month__prevmth');
-const daysElement = document.querySelector('.datepicker .datepicker__dates .datepicker__dates__days');
+const selectedDateElement = document.querySelector('.datepicker__dateselected');
+const datesElement = document.querySelector('.datepicker__dates');
+const mthElement = document.querySelector('.datepicker__dates__month__mth');
+const nextMthElement = document.querySelector('.datepicker__dates__month__nextmth');
+const prevMthElement = document.querySelector('.datepicker__dates__month__prevmth');
+const daysElement = document.querySelector('.datepicker__dates__days');
 
-//Alle måneder
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'];
 
-//Dato metoder
+//Oppretter et nytt dato-objekt, og metoder som henter dato, måned og år
 let date = new Date();
 let day = date.getDate();
 let month = date.getMonth();
 let year = date.getFullYear();
-
 
 let selectedDate = date;
 let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-//Valgt dato
+let hour = date.getHours();
+let minute = date.getMinutes();
+selectedDate.setHours(hour, minute);
+setTime();
+
+//Legger til måned og år i mthElement
 mthElement.textContent = months[month] + ' ' + year;
 
+//Legger til valgt dato til selectedDateElement-div og dataset.value leser verdien
 selectedDateElement.textContent = formatDate(date);
-selectedDateElement.dataset.value = selectedDate;
+//selectedDateElement.dataset.value = selectedDate;
 
+
+//Fyller datoer i kaledeneren
 populateDates();
 
-// EVENT LISTENERS
+//Event listeners som trigger funksjoner ved klikk
 datepickerElement.addEventListener('click', toggleDatePicker);
 nextMthElement.addEventListener('click', goToNextMonth);
 prevMthElement.addEventListener('click', goToPrevMonth);
 
-// FUNCTIONS
+//
 function toggleDatePicker (e) {
 	if (!checkEventPathForClass(e.path, 'dates')) {
 		datesElement.classList.toggle('active');
 	}
 }
 
+//Øker "månedstall" og årstall når funksjonen trigges 
 function goToNextMonth (e) {
-	month++;
+    month++;
 	if (month > 11) {
 		month = 0;
 		year++;
-	}
+    }
 	mthElement.textContent = months[month] + ' ' + year;
     populateDates();
     datesElement.classList.toggle('active');
 }
 
+//Minsker "månedstall" og årstall når funksjonen trigges 
 function goToPrevMonth (e) {
 	month--;
 	if (month < 0) {
@@ -64,38 +82,35 @@ function goToPrevMonth (e) {
     datesElement.classList.toggle('active');
 }
 
-function leapyear(year) {
-    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
-}
-
-var leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
-//Fyller inn dato i 
+//Fyller inn datoer i kalenderen
 function populateDates (e) {
     daysElement.innerHTML = '';
-    var leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
-    let amount_days = [31,28,30,29];
+    let amountDaysInMonth = [31,28,30,29];
 
-    if (month == 1){
-        amount_days = amount_days[1];
-    } else if ( month == 3 || month == 5 || month ==  8 || month ==  10 ) {
-        amount_days = amount_days[2];
-    } else if ((month == 1) && leapYear) {
-        amount_days = amount_days[3];
+//Justerer antall dager som vises utifra ulike måneder og skuddår
+    if ( (month == 1) && (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+        amountDays = amountDaysInMonth[3];
+    } else if ( month == 1 ){ 
+        amountDays = amountDaysInMonth [1];
     } else {
-        amount_days = amount_days[0];
+        if ( month == 0 || month == 2 || month ==  4 || month ==  6 || month == 7 || month ==  9 || month ==  11) {
+            amountDays = amountDaysInMonth[0];
+        }else if ( month == 3 || month == 5 || month ==  8 || month ==  10 ) {
+            amountDays = amountDaysInMonth[2];
+        } 
     }
 
-
-	for (let i = 0; i < amount_days; i++) {
-		const day_element = document.createElement('div');
-		day_element.classList.add('day');
-		day_element.textContent = i + 1;
+//Lager div'er med datoer utifra antall dager i måneden og legger inn i daysElement
+	for (let i = 0; i < amountDays; i++) {
+		const dayElement = document.createElement('div');
+		dayElement.classList.add('day');
+		dayElement.textContent = i + 1;
 
 		if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month) {
-			day_element.classList.add('selected');
+			dayElement.classList.add('selected');
 		}
 
-		day_element.addEventListener('click', function () {
+		dayElement.addEventListener('click', function () {
 			selectedDate = new Date(year + '-' + (month + 1) + '-' + (i + 1));
 			selectedDay = (i + 1);
 			selectedMonth = month;
@@ -107,7 +122,7 @@ function populateDates (e) {
 			populateDates();
 		});
 
-		daysElement.appendChild(day_element);
+		daysElement.appendChild(dayElement);
 	}
 }
 
@@ -122,19 +137,113 @@ function checkEventPathForClass (path, selector) {
 	return false;
 }
 
+
 function formatDate (d) {
 	let day = d.getDate();
 	if (day < 10) {
 		day = '0' + day;
 	}
 
-    let month = d.getMonth() + 1;
-    
-	if (month < 10) {
-		month = '0' + month;
-	}
-
 	let year = d.getFullYear();
 
-	return day + ' / ' + month + ' / ' + year;
+	return day + ' . ' + months[month].toLowerCase() + ' ' + year + " // " + time ;
 }
+
+
+
+
+
+//let time = timepickerElement.dataset.time;
+/*let hour = date.getHours();
+let minute = date.getMinutes();
+selectedDate.setHours(hour, minute);
+setTime();*/
+
+// EVENT LISTENERS
+hrUpElement.addEventListener('click', hour_up);
+hrDownElement.addEventListener('click', hour_down);
+
+minUpElement.addEventListener('click', minute_up);
+minDownElement.addEventListener('click', minute_down);
+
+hrElement.addEventListener('change', hour_change);
+minElement.addEventListener('change', minute_change);
+
+function hour_change (e) {
+	if (e.target.value > 23) {
+		e.target.value = 23;
+	} else if (e.target.value < 0) {
+		e.target.value = '00';
+	}
+
+	if (e.target.value == "") {
+		e.target.value = formatTime(hour);
+	}
+
+	hour = e.target.value;
+}
+
+function minute_change (e) {
+	if (e.target.value > 59) {
+		e.target.value = 59;
+	} else if (e.target.value < 0) {
+		e.target.value = '00';
+	}
+
+	if (e.target.value == "") {
+		e.target.value = formatTime(minute);
+	}
+
+	minute = e.target.value;
+}
+
+function hour_up () {
+	hour++;
+	if (hour > 23) {
+		hour = 0;
+	}
+	setTime();
+}
+function hour_down () {
+	hour--;
+	if (hour < 0) {
+		hour = 23;
+	}
+	setTime();
+}
+
+function minute_up () {
+	minute++;
+	if (minute > 59) {
+		minute = 0;
+		hour++;
+	}
+    setTime();
+    
+}
+function minute_down () {
+	minute--;
+	if (minute < 0) {
+		minute = 59;
+		hour--;
+	}
+	setTime();
+}
+
+function setTime () {
+	hrElement.value = formatTime(hour);
+	minElement.value = formatTime(minute);
+    timepickerElement.dataset.time = formatTime(hour) + ':' + formatTime(minute);
+    time = formatTime(hour) + ':' + formatTime(minute);
+    selectedDate.setHours(hrElement.value, minElement.value);
+    //return day + ' . ' + months[month].toLowerCase() + ' ' + year + " // " + time ;
+    
+}
+
+function formatTime (time) {
+	if (time < 10) {
+		time = '0' + time;
+	}
+	return time;
+}
+
