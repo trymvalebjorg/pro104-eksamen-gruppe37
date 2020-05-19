8dd7904c1b5e22c1a4795e526aaa3c99f0bf89e3
trymvalebjorg <trymvalebjorg@gmail.com>
2020-05-19 10:30:51 +0200

    List CRUD, added js and css for a simple modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // ReadMore for task
 let toggler = document.getElementsByClassName("list__item__expand");
 for( i = 0; i < toggler.length; i++){
@@ -8,3 +7,70 @@ for( i = 0; i < toggler.length; i++){
         this.classList.toggle('arrow-down');
     })
 }
+
+/* Generating unique ID for new Input */
+function guid() {
+    return parseInt(Date.now() + Math.random());
+}
+
+/* Create and save a new list */
+function saveList() {
+    let keys = ['title'];
+    let obj = {};
+
+    keys.forEach(function (item) {
+        let result = document.getElementById(item).value;
+        if (result) {
+            obj[item] = result;
+        }
+    })
+
+    if (Object.keys(obj).length) {
+        let lists = getLists();
+        obj.id = guid();
+        lists.push(obj);
+        let data = JSON.stringify(lists);
+        localStorage.setItem("lists", data);
+        newList(obj);
+    }
+}
+
+/* Get All lists already stored into the local storage */
+function getLists() {
+    var listRecord = localStorage.getItem("lists");
+    var lists = [];
+    if (!listRecord) {
+        return lists;
+    } else {
+        lists = JSON.parse(listRecord);
+        return lists;
+    }
+}
+
+function newList(item) {
+
+    let main = document.getElementsByTagName('main')[0];
+    let newList = document.createElement('div');
+    let header = document.createElement('div');
+    let actionBtn = document.createElement('button');
+    let addTask = document.createElement('div');
+   
+    newList.className = 'list'; 
+   
+    header.className = 'list__header';
+    header.innerHTML = `
+        <img src="img/icons/album.svg" alt="an icon" class="icon">
+        <h3>${item.title}</h3>`;
+    
+    actionBtn.className = 'list__action-btn btn btn--round btn--add';
+    
+    addTask.className = 'list__item';
+    addTask.innerHTML = "Create a new task!"
+
+    main.appendChild(newList);
+    newList.appendChild(actionBtn);
+    newList.appendChild(header);
+    newList.appendChild(addTask);
+
+}
+
