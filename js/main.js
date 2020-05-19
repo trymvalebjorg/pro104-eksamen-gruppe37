7eda3608b8d3216c4bceeae672f400f87773a363
JSourianos <thomasourianos@live.no>
2020-05-19 10:22:01 +0200

    Just commented some code

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ for( i = 0; i < toggler.length; i++){
 }
 
 
-
 // ---------------- ADDING NEW TASK ---------------------------------
 //Everything is stored within an array in localStorage, so to access the data we have to use loops and indexes.
 
@@ -26,8 +25,8 @@ function addNewTask(event){
    
     //This is the input fields value
     const taskInput = listItemInput.value;
-    //Object which contains the key 'task' and the value of the user input
-    const taskObj = [{"task": taskInput}];
+ 
+    
     //pushing the object into localStorage array
     taskList.push({taskInput});
     //Sets the value in the input-field into localStorage
@@ -35,8 +34,6 @@ function addNewTask(event){
     
 }
 
-//When clicking the button, we add a new task to localStorage.
-listItemButton.onclick = addNewTask; 
 
 //This function is responsible for showing the tasks listed in localStorage
 function showTasks(){
@@ -45,18 +42,19 @@ function showTasks(){
 
     //Looping through the taskList array which contains the input from user.
     for(let i = 0; i < taskList.length; i++){
+        const listDiv = document.querySelector('.list__item');
         //Creating a new List item as a div
         const listItem = document.createElement('div');
         //Assigning attributes to the newly created div to access some of the CSS styling.
-        listItem.setAttribute('class', 'list__item');
+        listItem.setAttribute("class", "list__item");
 
         //Outputting the HTML into our newly created div
         //Maybe this can be more dynamic????
-        listItem.innerHTML += `
+        listDiv.innerHTML += `
             <figure class="list__item__importance dot dot--green"></figure>
             <div class="list__item__text">
                 <p class="list__item__text--date">16.05.2020</p>
-                <p class="list__item__text--task">${taskList[i]}</p>
+                <p class="list__item__text--task">${JSON.stringify(taskList[i].taskInput)}</p>
             </div>
 
             <div class="list__item__expand">
@@ -66,7 +64,6 @@ function showTasks(){
             <div class="list__item__expanded">
                 <p>lorem</p>
             </div> 
-         
         `;
         //Appending the listItem div to the outputDiv
         outputDiv.appendChild(listItem);
@@ -74,12 +71,59 @@ function showTasks(){
     
 }
 
+// --------------------- ADDING NEW MEMBER -----------------
+
+//Selects the key from localStorage
+const memberList = JSON.parse(localStorage.getItem("memberList")) || [];
+//Gets the input field.
+const memberInput = document.getElementById("member__input");
+//Submit button
+const memberInputBtn = document.getElementById("member__input__btn");
+//Member object, stored in local storage
+const memberObj = {
+    'name': memberInput
+}
+
+//FUNCTION for adding member
+function addMember(event){
+    event.preventDefault();
+    const memberName = memberInput.value;
+    //If the input field is empty, display an alert
+    if(!memberInput){
+        alert("You must input a name.");
+    } else {
+        //Adding the object to the memberList
+        memberList.push({memberName});
+        //Setting the item to localStorage
+        localStorage.setItem("memberList", JSON.stringify(memberList));
+        
+    }
+
+}
+
+// ---- ON CLICKS -----------
+//When clicking the button, we add a new task to localStorage.
+listItemButton.onclick = addNewTask; 
+//When clicking the button, we add a new member to localStorage.
+memberInputBtn.onclick = addMember;
+
+//Just a dummy for outputting the values, THIS HAS TO BE CHANGED
+const test = document.getElementById("test");
+
+//Function that loops through local storage and outputs the value into a P-tag(change this later)
+function showMembers(){
+    for(let i = 0; i < memberList.length; i++){
+        test.innerHTML += `${JSON.stringify(memberList[i].memberName)}, `;
+    }
+}
+
+
+
 //just keeping all the running functions in one place.
 function main(){
     showTasks();
+    showMembers();
 }
 
 // ---------------- MAIN FUNCTIONS -------------------------
 main();
-
-
