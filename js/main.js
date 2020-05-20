22e891864b854c0a9eb179c3db612ab16e8333e4
dannyblueman <dhb_90@hotmail.com>
2020-05-20 19:28:41 +0200

    Now possible to upload profile pictures. Also fixed button-related bugs.

--- js/main.js
+++ js/main.js
@@ -13,20 +13,20 @@ for( i = 0; i < toggler.length; i++){
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
 
-    // Loop through the FileList and render image files as thumbnails.
+    // Loop through the FileList and render image files as thumbnails
     for (var i = 0, f; f = files[i]; i++) {
 
-      // Only process image files.
+      // Only process image files
       if (!f.type.match('image.*')) {
         continue;
       }
 
       var reader = new FileReader();
 
-      // Closure to capture the file information.
+      // Closure to capture the file information
       reader.onload = (function(theFile) {
         return function(e) {
-          // Render thumbnail.
+          // Render thumbnail
           var span = document.createElement('span');
           span.innerHTML = ['<img class="thumb" src="', e.target.result,
                             '" title="', escape(theFile.name), '"/>'].join('');
@@ -36,7 +36,7 @@ function handleFileSelect(evt) {
         };
       })(f);
 
-      // Read in the image file as a data URL.
+      // Read in the image file as a data URL
       reader.readAsDataURL(f);
     }
   }
@@ -51,5 +51,50 @@ function handleFileSelect(evt) {
                             '" title="test"/>'].join('');
 
           document.getElementById('list').insertBefore(span, null);
+    
+    }
+
+// Function to upload profile pictures to localStorage
+function handleProfileSelect(evt) {
+    var profiles = evt.target.files; // FileList object
+
+    // Loop through the FileList and render image files as thumbnails
+    for (var i = 0, f; f = profiles[i]; i++) {
+
+      // Only process image files
+      if (!f.type.match('image.*')) {
+        continue;
+      }
+
+      var prof_reader = new FileReader();
+
+      // Closure to capture the file information
+      prof_reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail
+          var prof_span = document.createElement('prof_span');
+          prof_span.innerHTML = ['<img class="profile-pic" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+            
+          document.getElementById('profilepic__list').insertBefore(prof_span, null);
+          localStorage.setItem('img', e.target.result);
+        };
+      })(f);
+
+      // Read in the image file as a data URL
+      prof_reader.readAsDataURL(f);
+    }
+  }
+
+  document.getElementById('profiles').addEventListener('change', handleProfileSelect, false);
+
+
+  if(localStorage.img) { 
+
+         var prof_span = document.createElement('prof_span');
+          prof_span.innerHTML += ['<img class="profile-pic" src="', localStorage.img,
+                            '" title="test"/>'].join('');
+
+          document.getElementById('profilepic__list').insertBefore(prof_span, null);
     
     }
