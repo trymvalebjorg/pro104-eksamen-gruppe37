ce2d94a1ec33fbb9ffcc17a200b364cb0803c2a4
emi-louise97 <emilouise1997@hotmail.com>
2020-05-22 09:46:35 +0200

    Tested some things

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // ReadMore for task
+
+
 let toggler = document.getElementsByClassName("list__item__expand");
 for( i = 0; i < toggler.length; i++){
     toggler[i].addEventListener('click', function() {
@@ -6,4 +8,10 @@ for( i = 0; i < toggler.length; i++){
         this.parentElement.querySelector('.list__item__expanded__controls').classList.toggle('active');
         this.classList.toggle('arrow-down');
     })
+}
+let importanceDotElements = document.querySelectorAll(".dot");
+importanceDotElements.addEventListener('click', myFunction);
+
+function myFunction(){
+    e.target.console.log("Dot has been clicked");
 }
