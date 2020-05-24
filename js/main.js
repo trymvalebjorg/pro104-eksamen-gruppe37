2eeb52c8b93feb98feab36fccfcbf06eabc1dc8d
emi-louise97 <emilouise1997@hotmail.com>
2020-05-24 12:13:19 +0200

    Added change dot color functionality. Removing popup menu not working yet.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 // ReadMore for task
-
-
 let toggler = document.getElementsByClassName("list__item__expand");
 for( i = 0; i < toggler.length; i++){
     toggler[i].addEventListener('click', function() {
@@ -10,25 +8,64 @@ for( i = 0; i < toggler.length; i++){
     })
 }
 
+let selectedDot;
+let importanceMenuElements;
+let selectedImportance;
+let notImportant;
+let veryImportant;
+let important;
+
 function addImportanceMenu(){
-    let selectedDot = this.document.querySelector('.dot');
+    selectedDot = this.document.querySelector('.dot');
         selectedDot.innerHTML = `
             <div class="list__item__importance__menu">
                 <h5 class="list__item__importance__menu__header">Velg prioritering</h5>
-                <figure class="list__item__importance__menu__dot dot--green not-important" onclick="changeImportance()"></figure>
-                <p class="list__item__importance__menu__priority not-important" onclick="changeImportance()">Ikke viktig</p>
+                <figure class="list__item__importance__menu__dot dot--green not-important"></figure>
+                <p class="list__item__importance__menu__priority not-important">Ikke viktig</p>
                 <figure class="list__item__importance__menu__dot dot--yellow important"></figure>
-                <p class="list__item__importance__menu__priority important">Ganske viktig</p>
+                <p class="list__item__importance__menu__priority important" onclick=>Ganske viktig</p>
                 <figure class="list__item__importance__menu__dot dot--red very-important"></figure>
                 <p class="list__item__importance__menu__priority very-important">Veldig viktig</p>
             </div>
         `;
     
-    let importanceMenuElements = document.querySelector('.list__item__importance__menu');
+    importanceMenuElements = this.document.querySelector('.list__item__importance__menu');
+
     importanceMenuElements.classList.add('active');
-   
+    
+    overlayElement.classList.toggle('active');
+
+    notImportant = document.getElementsByClassName('not-important');
+    veryImportant = document.getElementsByClassName('very-important');
+    important = document.getElementsByClassName('important');
+
+    for( i = 0; i < notImportant.length; i++){
+        notImportant[i].addEventListener('click', changeImportance);
+    }
+    for( i = 0; i < veryImportant.length; i++){
+        veryImportant[i].addEventListener('click', changeImportance);
+    }
+    for( i = 0; i < important.length; i++){
+        important[i].addEventListener('click', changeImportance);
+    }
 }
 
 function changeImportance(){
-    console.log('ChangeImportance function triggered');
+        let selectedImportanceClass = event.target.classList[1];
+        let selectedDotClass = selectedDot.classList[2];
+
+        selectedDot.classList.remove(selectedDotClass);
+
+        if (selectedImportanceClass  === "dot--green" || selectedImportanceClass  === "not-important"){
+            selectedDot.classList.add('dot--green');
+            console.log("green added");
+
+        } else if (selectedImportanceClass === "dot--yellow" || selectedImportanceClass  === "important"){
+            selectedDot.classList.add('dot--yellow');
+            console.log("yellow added");
+
+        } else {
+            selectedDot.classList.add('dot--red');
+            console.log("red added");
+        }
 }
